35e297042fe61e6497b03b7c4713f0b29bbe89f7
MinimalCoder <changkhobietdieu@gmail.com>
2023-09-08 16:42:22 +0700

    first commit

--- lib/bai1buoi2.dart
+++ lib/bai1buoi2.dart
@@ -31,9 +31,10 @@ void main(List<String> args) {
   print(
       "Đ${str.substring(2, 15)} buổi ${str.substring(25, 35)}về Dart: ${str.substring(44, 49).replaceAll(" ", "").toUpperCase()} BASIC (phần 1)");
 
-  print(str.replaceAll("của", "") +
-      str.replaceAll("basics", "BASIC") +
-      str.replaceAll("dart", "Dart") +
-      str.replaceAll("đây", "Đây") +
-      str.replaceAll("...", ""));
+  print(str
+      .replaceAll("của", "")
+      .replaceAll("basics", "BASIC")
+      .replaceAll("dart", "Dart")
+      .replaceAll("đây", "Đây")
+      .replaceAll("...", ""));
 }
