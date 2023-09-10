26bc3fe1d8b6e8f4849dd729c084da6b0ca63d0a
MinimalCoder <changkhobietdieu@gmail.com>
2023-09-10 14:42:44 +0700

    first commit

--- lib/buoi2/baitapngoai.dart
+++ lib/buoi2/baitapngoai.dart
@@ -1,7 +1,3 @@
-// ignore_for_file: avoid_print, unused_local_variable
-
-import 'package:flutter/material.dart';
-
 void main() {
   final phones = <String>['Nokia', 'Xiaomi', 'iPhone'];
 
@@ -10,9 +6,9 @@ void main() {
       '${identityHashCode(phones[0])} - ${identityHashCode(phones[1])} - ${identityHashCode(phones[2])}');
 
   // viết code ở đây, đổi vị trí Xiaomi và iPhone
-
-  phones[1] = "iPhone";
-  phones[2] = "Xiaomi";
+  String Doivitri = phones[1];
+  phones[1] = phones[2];
+  phones[2] = Doivitri;
   print(phones);
   print(
       '${identityHashCode(phones[0])} - ${identityHashCode(phones[1])} - ${identityHashCode(phones[2])}');
