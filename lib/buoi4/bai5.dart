2e87dceace8230f8bfffa2bd08173c4a069c8e15
MinimalCoder <changkhobietdieu@gmail.com>
2023-09-12 01:31:36 +0700

    first commit

--- lib/buoi4/bai5.dart
+++ lib/buoi4/bai5.dart
@@ -1,15 +1,35 @@
 void main() {
-  List<String> result = [];
-  for (int i = 0; i <= 99; i++)
-    switch (i % 2) {
-      case 0:
-        print("$i là số chẵn");
-        break;
-      case 1:
-        print("$i là số lẻ");
-        break;
-      default:
-        print("result");
-        break;
+  {
+    List<int> Evennumber = [];
+    List<int> Oddnumber = [];
+
+    /// Cách này copy của thầy e chỉ sửa chút xíu thôi
+    for (int i = 0; i <= 99; i++) {
+      switch (i % 2) {
+        case 0:
+          Evennumber.add(i);
+          break;
+        case 1:
+          Oddnumber.add(i);
+          break;
+        default:
+          print("result");
+          break;
+      }
     }
+    print("Số chẵn: $Evennumber");
+    print("Số lẻ: $Oddnumber");
+  }
+  List<int> evennumber = [];
+  List<int> oddnumber = [];
+
+  for (int i = 0; i <= 99; i++) {
+    if (i % 2 == 0) {
+      evennumber.add(i);
+    } else {
+      oddnumber.add(i);
+    }
+  }
+  print("Các số chẵn: $evennumber");
+  print("Các số lẻ: $oddnumber");
 }
